1518a9377f89edb06e3b724ee128718b9ccd3669
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-12-13 16:37:48 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import Nav from "./components/Nav";
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
     <section className="padding">PopularProducts</section>
     <section className="padding">SuperQuality</section>
